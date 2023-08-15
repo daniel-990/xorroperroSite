048fc591df1eac07695390f34408693ace30e5c7
daniel-990 <danielarango990@gmail.com>
2023-08-15 13:42:26 -0500

    secargan contenidos

--- main.js
+++ main.js
@@ -1,8 +1,52 @@
 //init
 const init = () => {
-    const data = "Hola mundo";
-    document.title = data;
-    console.log("Hola mundo");
+    const apiKey = "0ee6c1db7e356740c01f8648f5605c25";
+    const country = "3674962"; //MEDELLIN
+    const data = "http://api.openweathermap.org/geo/1.0/reverse?lat=6.2661496&lon=-75.5810686&limit=5&appid="+apiKey;
+    const data2 = "https://api.openweathermap.org/data/2.5/forecast?id="+country+"&appid="+apiKey;
+    const data3 = "http://api.openweathermap.org/data/2.5/find?q=CO&units=imperial&type=accurate&mode=json&appid=0ee6c1db7e356740c01f8648f5605c25";
+
+    const render = document.getElementById("render");
+    const renderDia = document.getElementById("render-dia");
+
+    // Hacer una petición para un usuario con ID especifico
+    axios.get(data2)
+    .then(function (response) {
+        const ciudad = response.data.city;
+        const datos = response.data.list;
+
+        console.log(ciudad);
+
+        // Cantidad de milisegundos
+        var luna = ciudad.sunset; // Cambia este valor a la cantidad que desees
+        var sol = ciudad.sunrise; // Cambia este valor a la cantidad que desees
+
+
+        render.innerHTML += `
+            <div class="contenedor-datos">
+                Ciudad Actual <br>${ciudad.name}
+            </div>
+        `;
+
+        if(sol < luna){
+            renderDia.innerHTML += `🌞`;
+            document.title = "🌞 Hola mundo";
+        }else{
+            renderDia.innerHTML += `🌚`;
+            document.title = "🌚 Hola mundo";
+        }
+
+        datos.map((item) => {
+            //console.log(item);
+        })
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+    .finally(function () {
+        // siempre sera executado
+    });
+
 }
 
 init();
